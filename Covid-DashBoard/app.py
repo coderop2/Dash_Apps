4a773aff6bb8776f235f0eb3f0ea72866e3ef498
coderop2 <harshitkhandelwal305@gmail.com>
2020-12-02 03:48:08 +0530

    Final App Draft

--- Covid-DashBoard/app.py
+++ Covid-DashBoard/app.py
@@ -31,15 +31,15 @@ divBorderStyle = {
 }
 
 #Creating custom style for local use
-boxBorderStyle = {
-    'borderColor' : '#393939',
-    'borderStyle': 'solid',
-    'borderRadius': '10px',
-    'borderWidth':2
-    # 'width': '100px',
-    # 'height': '50px'
-
-}
+boxBorderStyle = {'borderColor' : '#393939',
+                  'borderStyle': 'solid',
+                  'borderRadius': '10px',
+                  'borderWidth':2,
+                  'width': '15%',
+                  'text-align' : 'center',
+                  'margin-left' : '17.5%',
+                  'margin-right' : '17.5%'
+                                 }
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -50,6 +50,8 @@ data.drop(data[(data['new_cases'].isnull()) & (data['total_cases'].isnull())].in
 groups = data.groupby('location')
 
 df_pop = groups.agg({'population':'max','total_cases':'max','total_deaths':'max'})
+temp_df_pop = df_pop.reset_index()
+
 x = df_pop[['total_cases','total_deaths']]
 
 x.reset_index(inplace = True)
@@ -61,9 +63,10 @@ top_ten = x[:10]
 
 countries = data.location.unique()
 ten_count = top_ten.Country
+selected_country = x.loc[0].Country
 
 def getMainPlot():
-    fig = px.line(data.query("location in @ten_count"), x="date", y="total_cases", color = "location")#, height=400, width=1100)
+    fig = px.line(data.query("location in @ten_count"), x="date", y="total_cases", color = "location", color_discrete_sequence =["#e3f2fd","#bbdefb","#90caf9","#64b5f6","#42a5f5",'#2196f3','#1e88e5','#1976d2','#1565c0','#0d47a1'])#, height=400, width=1100)
     # fig.update_traces(mode='markers+lines')
     fig.update_xaxes(showgrid=True, gridwidth=2, gridcolor='#363636')
     fig.update_yaxes(showgrid=True, gridwidth=2, gridcolor='#363636')
@@ -150,7 +153,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                                                 'maxWidth': 0,
                                                 'fontSize':14},
                                     style_table={
-                                                 'maxHeight': '350px',
+                                                 'maxHeight': '400px',
                                                  'overflow-y': 'auto'
                                                 },
                                     style_data={
@@ -164,22 +167,25 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
               id='daily-count',
               figure=getMainPlot())
         ],style={'width': '70%', 'display': 'inline-block', 'float': 'right'}),
-    ], className = 'row'),
-    html.Div([
-              html.Span(
-                       style={'color': colors['text']},
-                       id='my-country'),
-              html.Span(
-                        style={'color': colors['confirmed_text'],
-                        'fontWeight': 'bold'},
-                        id='my-confirmed'),
-              html.Span(" with deaths: ",
-                       style={'color': colors['text']}),
-              html.Span(
-                        style={'color': colors['deaths_text'],
-                        'fontWeight': 'bold'},
-                        id='my-deaths')
-             ], style={'padding-top': '20px'}, className = 'row'),
+    ], className = 'row', style = {'margin-top': '2%'}),
+    
+    # html.Div([
+    #           html.Span(
+    #                    style={'color': colors['text']},
+    #                    id='my-country'),
+    #           html.Span(
+    #                     style={'color': colors['confirmed_text'],
+    #                     'fontWeight': 'bold'},
+    #                     id='my-confirmed'),
+    #           html.Span("<br>",
+    #                     id='testing'),
+    #           html.Span(" with deaths: ",
+    #                    style={'color': colors['text']}),
+    #           html.Span(
+    #                     style={'color': colors['deaths_text'],
+    #                     'fontWeight': 'bold'},
+    #                     id='my-deaths')
+    #          ], style={'padding-top': '20px'}, className = 'row'),
     
     html.Div([html.H2(id = 'countryHeader')], 
              id = 'countryName', 
@@ -190,24 +196,35 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     
     html.Div([
                 html.Div([html.H6("Cases : ",
-                                 style={'color': colors['confirmed_text']})
-                         ],style={'borderColor' : '#393939',
-                                  'borderStyle': 'solid',
-                                  'borderRadius': '10px',
-                                  'borderWidth':2
-                                  # 'width': '200px',
-                                  # 'height': '50px','margin-left'
-                                 }, className='two columns'),
+                                 style={'color': colors['confirmed_text']}),
+                          html.Div([
+                                html.P([
+                                    html.Span("Total Cases : ", style = {'color':colors['figure_text']}),
+                                    html.Span(id = 'country_cases', style = {'color' : 'lightblue'}),
+                                    html.Span(id = 'country_prev_day_cases', style = {'color' : 'blue'})
+                                ], style = {'text-align':'center'}),
+                                html.P([
+                                    html.Span("Cases Per Million : ", style = {'color':colors['figure_text']}),
+                                    html.Span(id = 'country_per_million_cases', style = {'color' : 'lightblue'})
+                                ], style = {'text-align':'center'})
+                            ])
+                         ],style=boxBorderStyle, className='two columns'),
 
 
                  html.Div([html.H6("Deaths : ",
-                                 style={'color': colors['deaths_text']})
-                         ],style={'borderColor' : '#393939',
-                                  'borderStyle': 'solid',
-                                  'borderRadius': '10px',
-                                  'borderWidth':2,
-                                  # 'width': '200px','height': '50px'
-                                 }, className='two columns')
+                                 style={'color': colors['deaths_text']}),
+                           html.Div([
+                                html.P([
+                                    html.Span("Total Deaths : ", style = {'color':colors['figure_text']}),
+                                    html.Span(id = 'country_deaths', style = {'color' : 'orange'}),
+                                    html.Span(id = 'country_prev_day_deaths', style = {'color' : 'red'})
+                                ], style = {'text-align':'center'}),
+                                html.P([
+                                    html.Span("Deaths Per Million : ", style = {'color':colors['figure_text']}),
+                                    html.Span(id = 'country_per_million_deaths', style = {'color' : 'red'})
+                                ], style = {'text-align':'center'})
+                            ])
+                         ],style=boxBorderStyle, className='two columns')
     ], className = 'row'),
     
     html.Div([
@@ -217,9 +234,8 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                     value = 'line',
                     labelStyle={'display': 'inline-block', 'color':colors['figure_text']},
                     id = 'totalCases_radio')
-                  ], className = 'select_type'),
-        html.Div([dcc.Graph(id='total-cases-country-plot')], 
-                 className = 'eachCountry'),
+                  ,dcc.Graph(id='total-cases-country-plot')], 
+                 className = 'eachCountry',style={'width': '30%','display': 'inline-block', 'margin-left':'2%'}),
         
         html.Div([dcc.RadioItems(
                     options = [{'label': 'Line Plot', 'value': 'line'},
@@ -227,9 +243,8 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                     value = 'line',
                     labelStyle={'display': 'inline-block', 'color':colors['figure_text']},
                     id = 'newCases_radio')
-                  ], className = 'select_type'),
-        html.Div([dcc.Graph(id='new-cases-country-plot')], 
-                 className = 'eachCountry'),
+                  ,dcc.Graph(id='new-cases-country-plot')], 
+                 className = 'eachCountry',style={'width': '30%','display': 'inline-block', 'margin-left':'3%', 'margin-right':'3%'}),
         
         html.Div([dcc.RadioItems(
                     options = [{'label': 'Line Plot', 'value': 'line'},
@@ -237,48 +252,76 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                     value = 'line',
                     labelStyle={'display': 'inline-block', 'color':colors['figure_text']},
                     id = 'totalDeaths_radio')
-                  ], className = 'select_type'),
-        html.Div([dcc.Graph(id='death-line-country-plot')], 
-                 className = 'eachCountry')] 
-        , className = 'row')
+                  ,dcc.Graph(id='death-line-country-plot')], 
+                 className = 'eachCountry',style={'width': '30%','display': 'inline-block', 'margin-right':'2%'})] 
+        , className = 'row', style = {'margin-top': '2%'})
 ], className = 'all_cols')
 
-
-
 @app.callback(
-    [Output('my-country', component_property = 'children'),
-     Output('my-confirmed', component_property = 'children'),
-    Output('my-deaths', component_property = 'children'),
-    Output('countryHeader', component_property = 'children')],
+    [Output('country_cases', component_property = 'children'),
+     Output('country_prev_day_cases', component_property = 'children'),
+     Output('country_per_million_cases', component_property = 'children'),
+     Output('country_deaths', component_property = 'children'),
+     Output('country_prev_day_deaths', component_property = 'children'),
+     Output('country_per_million_deaths', component_property = 'children'),
+     Output('countryHeader', component_property = 'children')],
     [Input('countries', 'selected_rows')])
-def getCountrySpecificData(selected_rows):
-    value = ""
-    if selected_rows is not None:
-        value = x.loc[selected_rows[0]].Country
-    else:
-        value = x.loc[0].Country
-    temp = data[data['location'] == str(value)]
+def getGetCountrySpecificInfo(selected_country, death = False):
+    country = x.loc[0].Country
+    if selected_country is not None:
+        country = x.loc[selected_country[0]].Country
+    country_group = groups.get_group(country)
+
+    temp = x[x['Country'] == country]
+    prev_day = country_group[country_group['date'] == '11-02-2020']
+
+    obj1 = str(temp['Total Cases'].values[0])
+    obj2 = "+" + str(prev_day['new_cases'].values[0])
+    obj3 = "%8.2f"%(round((temp['Total Cases'].values[0]/country_group.population.values[0])*1000000,2))
+    obj4 = str(temp['Total Deaths'].values[0])
+    obj5 = "+" + str(prev_day['new_deaths'].values[0])
+    obj6 = "%8.2f"%(round((temp['Total Deaths'].values[0]/country_group.population.values[0])*1000000,2))
+    return obj1, obj2, obj3, obj4, obj5, obj6, str(country)
+# @app.callback(
+#     [Output('my-country', component_property = 'children'),
+#      Output('my-confirmed', component_property = 'children'),
+#     Output('my-deaths', component_property = 'children'),
+#     Output('countryHeader', component_property = 'children')],
+#     [Input('countries', 'selected_rows')])
+# def getCountrySpecificData(selected_rows):
+#     country = x.loc[0].Country
+#     if selected_rows is not None:
+#         country = x.loc[selected_rows[0]].Country
+#     else:
+#         country = x.loc[0].Country
+#     temp = data[data['location'] == str(country)]
     
-    return str(value), str(temp.total_cases.max()), str(temp.total_deaths.max()), str(value)
+#     return str(country), str(temp.total_cases.max()), str(temp.total_deaths.max()), str(country)
 
 @app.callback(
     [Output('total-cases-country-plot','figure'),
-    Output('death-line-country-plot','figure'),
-    Output('new-cases-country-plot','figure')],
-    [Input('countries','selected_rows')])
-def plotCountrySpecificData(selected_rows):
-    country = ""
+    Output('new-cases-country-plot','figure'),
+    Output('death-line-country-plot','figure')],
+    [Input('countries','selected_rows'),
+    Input('totalCases_radio', 'value'),
+    Input('newCases_radio', 'value'),
+    Input('totalDeaths_radio', 'value')])
+def plotCountrySpecificData(selected_rows, *values):
+    country = x.loc[0].Country
     if selected_rows is not None:
         country = x.loc[selected_rows[0]].Country
-    else:
-        country = x.loc[0].Country
     
     temp = data[data['location'] == country]
+    selected_country = country
     
     plots = []
     
-    for col, color in zip(['total_cases','total_deaths','new_cases'],['#3CA4FF','#BB2205','#2d6187']):
-        pxfig = px.line(temp, x='date', y=col, color_discrete_sequence = [color])
+    for col, color, value in zip(['total_cases','new_cases','total_deaths'],['#3CA4FF','#2d6187','#BB2205'], values):
+        if value == "bar":
+            pxfig = px.bar(temp, x='date', y=col, color_discrete_sequence = [color])
+        else:
+            pxfig = px.line(temp, x='date', y=col, color_discrete_sequence = [color])
+            pxfig.update_traces(mode='markers+lines')
         pxfig.update_xaxes(showgrid=True, gridwidth=2, gridcolor=colors['gridcolor'])
         pxfig.update_yaxes(showgrid=True, gridwidth=2, gridcolor=colors['gridcolor'])
         pxfig.update_layout(xaxis_title = None,
@@ -294,7 +337,6 @@ def plotCountrySpecificData(selected_rows):
                            paper_bgcolor=colors['background'],
                            plot_bgcolor=colors['background'],
                            margin=dict(l=0, r=0, t=0, b=0))
-        pxfig.update_traces(mode='markers+lines')
         plots.append(pxfig)
     
     return plots[0], plots[1], plots[2]
